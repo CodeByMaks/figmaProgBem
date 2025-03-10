9ea4d2c2e8c9137e8e50f5377c16535bec18ae3e
CodeByMaks <maks667741@gmail.com>
2025-03-10 23:41:56 +0500

    fix some bugs

--- my-app/src/app/App.jsx
+++ my-app/src/app/App.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../styles/global.css'
-import Home from '../pages/home/home'
+import Home from '../pages/home/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
